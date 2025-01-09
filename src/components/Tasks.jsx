5f72887e7b5edb907faa2b7fbb39377d68c63b45
Victor Morais <victor.manso@outlook.com.br>
2025-01-09 18:27:15 -0300

    Função p/ atualizar a lista
    criei a função para atualizar a lista de tarefas e importei a função onTaskClick para o componente Tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,7 +5,12 @@ function Tasks(props) {
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 w-full text-white p-2 rounded-md text-left">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 w-full text-white p-2 rounded-md text-left ${
+              task.isComplited && "line-through"
+            }`}
+          >
             {task.title}
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
